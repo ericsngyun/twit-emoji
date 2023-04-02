3dcb4c86356c5a659370b74fe801da86c3423a58
Eric Yun <yunseric@gmail.com>
2023-04-01 23:24:07 -0700

    added signin signout btns

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
 
 export default withClerkMiddleware((req: NextRequest) => {
+    console.log('test')
   return NextResponse.next();
 });
 
