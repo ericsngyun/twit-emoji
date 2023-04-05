a0f5a2bf287de9c0512595f93030b59c3e29dd25
Eric Yun <yunseric@gmail.com>
2023-04-04 23:43:21 -0700

    user query

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,17 @@
+import { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+
+  return {
+    id: user.id, 
+    username: user.username,
+    profilePicture: user.profileImageUrl
+  }
+}
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
